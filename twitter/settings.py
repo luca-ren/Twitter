1ccb6c4440c03dac27dc3662b7e6dc05a4a5f7f8
luca-ren <renluca93@gmail.com>
2020-12-09 20:54:43 +0100

    Update settings.py
    erased Secret key

--- twitter/settings.py
+++ twitter/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1z!o!n55z#n7p(q05e(^2gis7octylxuh3_zujrja6^59&%4=m'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
